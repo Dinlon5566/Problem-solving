d6a7a38fd78c65c3b25ad126f29fca22583dc7c1
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2021-07-18 03:24:54 +0800

    update

---
+++ LeetCode/0283_Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeroCount=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                zeroCount++;
+            }else{
+                nums[i-zeroCount]=nums[i];
+            }
+        }
+        for(int i=nums.size()-zeroCount;i<nums.size();i++){
+            nums[i]=0;
+        }
+    }
+};

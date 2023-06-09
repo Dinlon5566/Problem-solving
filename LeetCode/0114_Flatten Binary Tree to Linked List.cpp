f38455f295441a0b8865f9f1fd25b8d706888c16
Dinlon5566 <ton200168@gmail.com>
2023-06-09 16:45:24 +0800

    update

---
+++ LeetCode/0114_Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* pre=nullptr;
+    void flatten(TreeNode* root) {
+        if(root==nullptr){
+            return;
+        }
+        flatten(root->right);
+        flatten(root->left);
+        root->right=pre;
+        root->left=nullptr;
+        pre=root;
+    }
+
+};
